3b324f6e822a7ce3d4b65b91b149ed505392ac9f
bnvenky <bnvenkatesheee@gmail.com>
2024-07-05 10:23:34 +0530

    update initial commit app

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,29 +5,23 @@ import './index.css';
 
 
 const Home = () => {
-  
-  // Use Navigate for redirection
-  /*if (jwtToken === undefined) {
-    return <Navigate to="/login" />;
-  }*/
 
   return (
     <>
       
       <div className="home-container">
         <div className="home-content">
-          <h1 className="home-heading">Clothes That Get YOU Noticed</h1>
+          <h1 className="home-heading">Clothes That Make You Stand Out</h1>
           <img
             src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
-            alt="clothes that get you noticed"
+            alt="clothes that make you stand out"
             className="home-mobile-img"
           />
           <p className="home-description">
-            Fashion is part of the daily air and it does not quite help that it
-            changes all the time. Clothes have always been a marker of the era
-            and we are in a revolution. Your fashion makes you been seen and
-            heard that way you are. So, celebrate the seasons new and exciting
-            fashion in your own way.
+          Fashion is part of everyday life and it's always changing. 
+          Clothes reflect the times and we're in a fashion revolution. 
+          What you wear makes a statement about who you are. So, embrace the new and exciting 
+          trends each season in your own unique way.
           </p>
           <Link to="/products">
             <button type="button" className="shop-now-button">
